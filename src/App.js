051865d604822cb9f0e40de2435372eb57ef7f01
Daniel Andrés Vélez Vargas <daniel_andres.velez@uao.edu.co>
2024-06-14 09:56:26 -0500

    [ADD] custom hook for handling localStorage logic

--- src/App.js
+++ src/App.js
@@ -7,6 +7,30 @@ import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
 import { useState } from "react";
+
+// *Creación de un custom hook para manejar la lógica de uso del localStorage
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
+  }
+
+  const [item, setItem] = useState(parsedItem);
+
+  // Función para actualizar el estado de los TODOs en el localStorage y en el estado de React.
+  const updateItem = (newItem) => {
+    localStorage.setItem(itemName, JSON.stringify(newItem));
+    setItem(newItem);
+  };
+
+  return [item, updateItem];
+}
+
 // Función usada para quitar las tildes de los TODO's que las tengan y así permitir búsquedas con estos.
 function normalizeText(text) {
   return text
@@ -16,18 +40,7 @@ function normalizeText(text) {
 }
 
 function App() {
-  const localStorageTodos = localStorage.getItem("TODOS");
-  let parsedTodos;
-
-  if (!localStorageTodos) {
-    localStorage.setItem("TODOS", JSON.stringify([]));
-    parsedTodos = [];
-  } else {
-    parsedTodos = JSON.parse(localStorageTodos);
-  }
-
-  // Seteamos los TODO's por defecto para contar con un "placeholder" en el estado.
-  const [todos, setTodos] = useState(parsedTodos);
+  const [todos, updateTodos] = useLocalStorage("TODOS", []);
   const [todoQuery, setTodoQuery] = useState("");
 
   // *Estados derivados: estos estados surgen de estados que ya han sido previamente declarados.
@@ -40,12 +53,6 @@ function App() {
     const queryText = normalizeText(todoQuery.toLowerCase());
     return todoText.includes(queryText);
   });
-
-  // Función para actualizar el estado de los TODOs en el localStorage y en el estado de React.
-  const updateTodos = (newTodos) => {
-    localStorage.setItem("TODOS", JSON.stringify(newTodos));
-    setTodos(newTodos);
-  };
 
   // *Manejo de completado y eliminación de TODO's.
   const completeTodo = (text) => {
@@ -70,21 +77,23 @@ function App() {
 
   return (
     // El Fragment permite que se envuelvan todos los elementos HTML o componentes (en este caso) en un envoltorio padre (como se suele solicitar por React), pero a la hora de renderizar en el navegador, se podrán inspeccionar los elementos HTML por completo y no renderizará un solo elemento que envuelva a todo y no lo permita observar. Se puede escribir como "fragment" o dejar solo los caimanes.
-    <div className='container'>
-      <TodoCounter completed={completedTodos} total={totalTodos} />
-      <TodoSearch todoQuery={todoQuery} setTodoQuery={setTodoQuery} />
+    <div>
+      <div className='container'>
+        <TodoCounter completed={completedTodos} total={totalTodos} />
+        <TodoSearch todoQuery={todoQuery} setTodoQuery={setTodoQuery} />
 
-      <TodoList>
-        {searchedTodos.map((todo) => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
+        <TodoList>
+          {searchedTodos.map((todo) => (
+            <TodoItem
+              key={todo.text}
+              text={todo.text}
+              completed={todo.completed}
+              onComplete={() => completeTodo(todo.text)}
+              onDelete={() => deleteTodo(todo.text)}
+            />
+          ))}
+        </TodoList>
+      </div>
 
       <CreateTodoButton />
     </div>
